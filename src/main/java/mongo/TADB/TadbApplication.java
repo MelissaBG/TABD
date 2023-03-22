040fc8bad4fc860f5c31984cfb094ab019986753
Melissa Bernardes Goulart <91767583+MelissaBG@users.noreply.github.com>
2023-03-22 10:35:57 -0300

    22-03-2023

---
+++ src/main/java/mongo/TADB/TadbApplication.java
@@ -0,0 +1,18 @@
+package mongo.TADB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadbApplication {
+
+	public static void main(String[] args) {
+
+		try {
+			SpringApplication.run(TadbApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/mongo/TADB/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package mongo.TADB.repository;
+
+
+import mongo.TADB.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
